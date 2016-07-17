d9a11a9897fc783d4556b0ca672d9ab67d68494a
perryraskin <perryraskin@gmail.com>
2016-07-17 10:25:00 +0300

    Read the file from standard input instead of prompting the user for a filename, and copy JSON data to CSV file

--- json_saver.py
+++ json_saver.py
@@ -2,18 +2,27 @@
 
 import json
 import sys
+import csv
+import time
 
 def json_in(fh):
     contents = json.load(fh)
-    print "Here's your file:"
-    print contents
+    return contents
 
-def to_csv():
-    pass
+def to_csv(parsed_input):
+    # Create csv file and write data to it
+    keys = parsed_input.keys()
+    csv_file_name = time.strftime("%d_%m_%Y") + "_data.csv"
+
+    with open(csv_file_name, 'w') as csvfile:
+        dict_writer = csv.DictWriter(csvfile, fieldnames=['height', 'weight', 'name'])
+        #dict_writer = csv.DictWriter(csvfile, fieldnames=keys)
+        dict_writer.writeheader() # writing header in the csv file
+        dict_writer.writerow(parsed_input)
 
 def main():
-    json_in(sys.stdin)
-	
+    user_input = json_in(sys.stdin)
+    to_csv(user_input)
 
 if __name__ == '__main__':
     main()
