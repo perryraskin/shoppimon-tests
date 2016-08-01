f985473a6a692a2f9ed9901ac30b55d0790fa728
perryraskin <perryraskin@gmail.com>
2016-08-01 10:37:56 +0300

    Read config.yaml, and display to which services the user events [based on user_event.json] will be sent.

--- user_event_json.py
+++ user_event_json.py
@@ -6,6 +6,7 @@ import json
 import sys
 import csv
 import time
+import yaml
 
 app = Flask(__name__)
 
@@ -18,7 +19,7 @@ def to_csv(parsed_input):
         csv_file_name = str(parsed_input['user_id']) + "_" + time.strftime("%d_%m_%Y") + "_data.csv"
     except KeyError:
         csv_file_name = str(parsed_input['account_id']) + "_" + time.strftime("%d_%m_%Y") + "_data.csv"
-        
+
 
     with open(csv_file_name, 'w') as csvfile:
         #dict_writer = csv.DictWriter(csvfile, fieldnames=['user_id', 'height', 'weight', ])
@@ -29,21 +30,32 @@ def to_csv(parsed_input):
     return csv_file_name + "\n"
 
 
+def print_correct_string(json_event):
+    with open("config.yaml", 'r') as stream:
+        try:
+            router_settings = yaml.load(stream)
+        except yaml.YAMLError as exc:
+            print exc
+
+    if json_event['event'] in router_settings['routing']:
+        for service in router_settings['routing'][json_event['event']]:
+            print "Sending {event} event to {services} service.".format(event = json_event['event'], services = service) 
+
+
 @app.route('/user_events', methods=['POST'])
 def save_user_event():
     data = request.json
     if ('user_id' not in data.keys()) and ('account_id' not in data.keys()):
         return 'Error! user_id or account_id field is missing\n'
-    if 'event' not in data.keys():
+    elif 'event' not in data.keys():
         return 'Error! event field is missing\n'
-    if 'source' not in data.keys():
+    elif 'source' not in data.keys():
         return 'Error! source field is missing\n'
     else:   
         filename = to_csv(data)
+        print_correct_string(data)
         return filename
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
