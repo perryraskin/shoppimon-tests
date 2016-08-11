c0331edf67ae9c909d7b50440206b542f3a95607
perryraskin <perryraskin@gmail.com>
2016-08-11 12:03:55 +0300

    Configuration module: Read config.yml and return its data.

--- config.py
+++ config.py
@@ -2,12 +2,30 @@
 """
 
 import yaml
+import boto3
 
 __config = None
 
-def get_config():
+def get_config(path):
     global __config
-    if __config is None:
-      with open("config.yaml", 'r') as stream:
-          __config = yaml.load(stream)
+
+    if path.startswith("s3"):
+        # Use Amazon S3
+        s3 = boto3.resource('s3')
+
+        bucket_name = path.split("/")[2]
+        key_name = path.split(bucket_name[-1])
+
+        # Download object at bucket-name with key-name to tmp.txt
+        s3.download_file(bucket_name, key_name, "tmp.yaml")
+
+        if __config is None:
+            with open("tmp.yaml", 'r') as stream:
+                __config = yaml.load(stream)
+        
+    else:
+        if __config is None:
+            with open(path, 'r') as stream:
+                __config = yaml.load(stream)
+
     return __config
